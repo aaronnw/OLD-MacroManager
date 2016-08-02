a5f8e0d3ffc650d8da2eaee77b920e731f5aee9b
Aaron.Wilson <aaronnw11@gmail.com>
2016-08-02 17:11:54 -0500

    Added grid and macro object templates to hold and send macro data

---
+++ app/src/main/java/dev/wilson/aaron/macromanager/GridObject.java
@@ -0,0 +1,18 @@
+package dev.wilson.aaron.macromanager;
+
+/**
+ * Created by Aaron.Wilson on 8/2/2016.
+ */
+public class GridObject{
+    String[][] macros;
+
+    public GridObject(int rowSize, int columnSize){
+        macros = new String[rowSize][columnSize];
+        for(int i = 0; i <rowSize; i++){
+
+        }
+    }
+    public void addMacro(String command){
+
+    }
+}
---
+++ app/src/main/java/dev/wilson/aaron/macromanager/MacroObject.java
@@ -0,0 +1,10 @@
+package dev.wilson.aaron.macromanager;
+
+/**
+ * Created by Aaron.Wilson on 8/2/2016.
+ */
+public class MacroObject{
+    public void MacroObject(String title, String command){
+
+    }
+}
